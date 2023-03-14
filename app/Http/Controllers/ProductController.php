035fd9b242d995c00975a2b0cdd7b38cea1a5d13
nghialm <nghialm@bap.jp>
2023-03-14 10:56:05 +0700

    add demo resource'

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function getProduct(){
+        return ProductResource::collection(Product::all());
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true;
+
+    protected $table = 'product';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'image',
+        'describe',
+    ];
+
+   
+}
